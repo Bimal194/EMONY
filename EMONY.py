2a9d7193dd7d4b3cde94c37af74d4928c8459a56
Nikhil Moghe <nikhilmoghe1998@gmail.com>
2018-09-20 00:47:33 +0530

    Update EMONY.py

--- EMONY.py
+++ EMONY.py
@@ -3,20 +3,21 @@ from bs4 import BeautifulSoup
 
 
 def movies_in_theatre(url):
-    code=requests.get(url)
-    text=code.text
-    soup=BeautifulSoup(text,"html.parser")
-    movie_name="Movies_near_me @BookmyShow"+"\n\n"
-    i=1
-    for link in soup.findAll('a',{'class':'__movie-name'}):
-        title=link.get('title')
-        link="https://in.bookmyshow.com"+link.get('href')
-        movie_name+=str(i)+"."+title+" "+link+"\n"
-        i+=1
+    code = requests.get(url)
+    text = code.text
+    soup = BeautifulSoup(text, "html.parser")
+    movie_name = "Movies_near_me @BookmyShow" + "\n\n"
+    i = 1
+    for link in soup.findAll('div', {'class': 'card-container wow fadeIn movie-card-container'}):
+        title = link.find('a')['title']
+        link = "https://in.bookmyshow.com" + link.find('a')['href']
+        movie_name += str(i) + "." + title + " " + link + "\n"
+        i += 1
 
-    fw = open('Movies_in_my_city.txt', 'w')
+    fw = open('Movies_near_me.txt', 'w')
     fw.write(movie_name)
     fw.close()
 
-city=input("Enter a valid city name: ")
-movies_in_theatre("https://in.bookmyshow.com/"+city+"/movies")
+
+city = input("Enter a valid city name: ")
+movies_in_theatre("https://in.bookmyshow.com/" + city + "/movies")
